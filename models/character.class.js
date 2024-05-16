dbb2c911b644c429c9dec69f1e4ec606cd012c43
phischa <ph.git@mailbox.org>
2024-05-16 21:27:29 +0200

    7. commit

--- models/character.class.js
+++ models/character.class.js
@@ -4,6 +4,7 @@ class Character extends MoveableObject {
     height = 300;
     y = 135;
     speed = 10;
+    walking_sound = new Audio('audio/walking.mp3');
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -20,11 +21,13 @@ class Character extends MoveableObject {
     }
 
     animateWalk() {
-        setInterval(() =>{
-            console.log(this.world.level.level_end_x);
+        setInterval(() => {
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.playWalkingSound();
+            } else {
+                this.pauseWalkingSound();
             }
             if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
@@ -35,13 +38,28 @@ class Character extends MoveableObject {
 
         setInterval(() => {
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                this.x += this.speed;
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
         }, 100);
+    }
+
+    playWalkingSound() {
+        if (this.walking_sound.paused) {
+            this.walking_sound.play().catch(error => {
+                console.error('Fehler beim Abspielen des Sounds:', error);
+            });
+            this.walking_sound.addEventListener('ended', () => {
+                this.walking_sound.currentTime = 0;
+                this.walking_sound.play();
+            });
+        }
+    }
+
+    pauseWalkingSound() {
+        this.walking_sound.pause();
     }
 
     jump() {
