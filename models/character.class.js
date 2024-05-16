66fa7057a9c8028cfca4b89683e2c0117c300031
phischa <ph.git@mailbox.org>
2024-05-16 23:01:41 +0200

    8. commit

--- models/character.class.js
+++ models/character.class.js
@@ -2,7 +2,7 @@ class Character extends MoveableObject {
 
     world;
     height = 300;
-    y = 135;
+    y = 40;
     speed = 10;
     walking_sound = new Audio('audio/walking.mp3');
     IMAGES_WALKING = [
@@ -14,9 +14,23 @@ class Character extends MoveableObject {
         'img/2_character_pepe/2_walk/W-26.png'
     ];
 
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png'
+    ]
+
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
+        this.applyGravity();
         this.animateWalk();
     }
 
@@ -37,8 +51,12 @@ class Character extends MoveableObject {
         }, 1000 / 120);
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                this.playAnimation(this.IMAGES_WALKING);
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
             }
         }, 100);
     }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -8,6 +8,8 @@ class MoveableObject {
     currentImage = 0;
     speed = 0.2;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2;
 
     loadImage(path) {
         this.img = new Image();
@@ -37,5 +39,18 @@ class MoveableObject {
         setInterval(() => {
             this.x -= this.speed;
         }, 1000 / 60);
+    }
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround() {
+        return this.y < 120;
     }
 }
